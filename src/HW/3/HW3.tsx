399c1ed470cda196211507b12f3e67e615ceaca6
mityaycho <mityaycho@gmail.com>
2023-10-28 16:25:27 +0300

    add some changes

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -10,16 +10,16 @@ export const HW3 = () => {
   // - приложение должно компилироваться и запускаться в браузере
 
 
-  const [currentText, setCurrentText] = useState('');
-  const [texts, setTexts] = useState<string[]>([
-    'То, что вы делаете по ночам, то и делает вас богатым. (Аль Капоне)',
-  ]);
+  const [currentText, setCurrentText] = useState<string>("");
+  const [texts, setTexts] = useState<string[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setCurrentText(event.currentTarget.value);
+		setCurrentText(event.currentTarget.value);
   };
 
   const handleSave = () => {
+		const newText = [currentText];
+		setTexts([currentText, ...newText]);
     // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
     // А ЗАТЕМ УБРАТЬ ЗА СОБОЙ В currentText
   };
@@ -27,27 +27,27 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-       <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+       <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
        <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
 
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
 
-      <button id={'hw03-button'} onClick={() => {handleSave}}> // НЕ ХВАТАТЕТ ФУНКЦИИ
+      <button id={'hw03-button'} onClick={() => {handleSave()}}> // НЕ ХВАТАТЕТ ФУНКЦИИ
        Сохранить
       </button>
 
       <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
       <ol id={'hw03-tasks'}>
-       {/* {ОТРИСОВАТЬ МАССИВ.map((el, index) => {
+       {.map((el, index) => {
          return (
            <li key={index} id={`hw03-task-${index}`}>
              {el}
            </li>
          );
-       })} */}
+       })}
       </ol>
     </div>
   );
