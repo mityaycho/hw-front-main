92ea2ac1426abc6253d8b6080335f0b5b6b3a089
mityaycho <mityaycho@gmail.com>
2023-10-28 16:57:19 +0300

    add some changes

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent } from 'react';
 
 export const HW3 = () => {
   // 1️⃣ Раскомментируйте JSX(HW3.tsx) и вы увидите,
