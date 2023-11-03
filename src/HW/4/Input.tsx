f7d8e3e718af06895b0e87a1c8873e5f358330b5
mityaycho <mityaycho@gmail.com>
2023-11-04 00:03:41 +0300

    hw4 completed

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,16 +1,16 @@
 import { ChangeEvent } from "react";
 
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string; // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (event: string) => void; // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Input = (props: InputPropsType) => {
 	const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-		// НУЖНО ДОПИСАТЬ
+		props.setCurrentText(event.currentTarget.value);
 	};
 
 	return (
-	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+		<input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
 	);
 };
