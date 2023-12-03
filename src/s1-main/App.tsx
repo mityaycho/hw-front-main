78d595ae18bc1411a5b44f5ed540ab72535317e7
mityaycho <mityaycho@gmail.com>
2023-12-03 18:25:30 +0300

    hw4 completed

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import s from './App.module.css'
-import HW4 from '../s2-homeworks/hw04/HW4'
 
 function App() {
 	return (
@@ -9,7 +8,7 @@ function App() {
 			{/*раскомментировать по ходу выполнения*/}
 			{/* <HW2/> */}
 			{/* <HW3/> */}
-			<HW4/>
+			{/* <HW4/> */}
 
 			{/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
 			{/*<HW5/>*/}
