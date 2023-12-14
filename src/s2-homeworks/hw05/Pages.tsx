7f360f0ed05c8185bb426244c8dee91a6a404b78
mityaycho <mityaycho@gmail.com>
2023-12-14 22:08:00 +0300

    add route error

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -26,7 +26,7 @@ function Pages() {
                 <Route path='/junior-plus' element={<JuniorPlus />} />
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path='*' element={<Error404 />} />
             </Routes>
         </div>
     )
