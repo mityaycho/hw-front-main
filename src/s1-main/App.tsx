9a7b4c59a6608df71fed0cd04dacdb5684ce3403
mityaycho <mityaycho@gmail.com>
2023-12-27 18:56:27 +0300

    add some changes

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import s from './App.module.css'
-import HW6 from '../s2-homeworks/hw06/HW6'
 import HW5 from '../s2-homeworks/hw05/HW5'
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
 
 			{/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
 			<HW5/>
-			
 		</div>
 	)
 }
