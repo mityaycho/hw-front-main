4db740010e7d2dad5884c1a6354c644aad819310
mityaycho <mityaycho@gmail.com>
2024-02-08 22:15:12 +0300

    add sort down to homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,11 +7,14 @@ type ActionType =
 export const homeWorkReducer = (state: Array<UserType>, action: ActionType): UserType[] => { // need to fix any
 	switch (action.type) {
 		case 'sort': { // by name
-			state.sort((a: UserType, b: UserType): any => a.name > b.name)
+			if (action.payload === 'up') {
+				state.sort((a: UserType, b: UserType): any => a.name > b.name);
+			} else if (action.payload === 'down') {
+				state.sort((a: UserType, b: UserType): any => a.name < b.name);
+			}
 			return state // need to fix
 		}
 		case 'check': {
-
 			return state // need to fix
 		}
 		default:
